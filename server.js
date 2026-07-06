b6777fda786c6dfee7386b0a36f799b306fa2853
nzlf <2300016145@webmail.uad.ac.id>
2026-07-06 19:13:23 +0700

    update server js

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ app.use('/api/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
